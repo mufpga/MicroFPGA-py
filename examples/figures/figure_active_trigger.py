68cc71057f2df646479afb2f6f9834c8ca18892e
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2021-11-15 10:20:47 +0100

    Add new figure script

--- examples/figures/figure_active_trigger.py
+++ examples/figures/figure_active_trigger.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-""" Demonstrate active camera triggering with a single laser.
+""" Demonstrate active camera triggering with three lasers.
 """
 
 import microfpga.controller as cl
@@ -8,7 +8,7 @@ from microfpga.signals import LaserTriggerMode
 import time
 
 # use_camera=True and default_trigger=True are the defaults and could be omitted
-with cl.MicroFPGA(n_laser=1, use_camera=True, default_trigger=True) as mufpga:
+with cl.MicroFPGA(n_laser=3, use_camera=True, default_trigger=True) as mufpga:
     # check if successful
     if mufpga.is_connected():
 
@@ -22,8 +22,8 @@ with cl.MicroFPGA(n_laser=1, use_camera=True, default_trigger=True) as mufpga:
             # max(delay) = 0 to 655,35 ms <--- delay goes in steps of 0.01 ms
             camera = {
                 'pulse': 1,  # ms
-                'period': 50,
-                'exposure': 40,
+                'period': 31.5,
+                'exposure': 30,
                 'delay': 0.5  # delay of 500 us between camera pulse and start of the exposure
             }
             mufpga.set_camera_state_ms(**camera)  # set the values in ms
@@ -31,21 +31,35 @@ with cl.MicroFPGA(n_laser=1, use_camera=True, default_trigger=True) as mufpga:
             # print state
             print(mufpga.get_camera_state_ms())
 
-            # define one laser pulsing on rising edge of the camera trigger with pulse lengths 1 us.
+            # define three lasers.
             laser0 = {
                 'channel': 0,
+                'mode': LaserTriggerMode.MODE_FOLLOW.value,
+                'duration': 1,
+                'sequence': sig.format_sequence('1010101010101010')
+            }
+            laser1 = {
+                'channel': 1,
                 'mode': LaserTriggerMode.MODE_RISING.value,
-                'duration': 1,  # in us
-                'sequence': sig.MAX_SEQUENCE
+                'duration': 2000,
+                'sequence': sig.format_sequence('0101010101010101')
+            }
+            laser2 = {
+                'channel': 2,
+                'mode': LaserTriggerMode.MODE_FALLING.value,
+                'duration': 2000,
+                'sequence': sig.format_sequence('1100110011001100')
             }
             mufpga.set_laser_state(**laser0)
+            mufpga.set_laser_state(**laser1)
+            mufpga.set_laser_state(**laser2)
 
             # we also need to start the camera
             mufpga.start_camera()
             print('Camera running')
 
             # now the FPGA generates both camera and laser trigger for 15 s
-            time.sleep(15)  # in s
+            time.sleep(2)  # in s
 
             # stop, the trigger signals are off
             mufpga.stop_camera()
