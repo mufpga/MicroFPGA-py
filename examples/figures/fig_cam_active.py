fba01291ecda992d52aa3037b92615a543a5f350
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2022-03-31 15:31:36 +0200

    add new figure script and rename all

---
+++ examples/figures/fig_cam_active.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+""" Demonstrate active camera triggering with three lasers.
+"""
+
+import microfpga.controller as cl
+import microfpga.signals as sig
+from microfpga.signals import LaserTriggerMode
+import time
+
+# use_camera=True is default and could be omitted
+with cl.MicroFPGA(n_laser=1, use_camera=True) as mufpga:
+    # check if successful
+    if mufpga.is_connected():
+
+        # if we are in camera trigger mode
+        if mufpga.is_active_sync():
+            camera = {
+                'pulse': 1.5,  # ms
+                'delay': 0.5,
+                'exposure': 10,
+                'readout': 2,
+            }
+            mufpga.set_camera_state_ms(**camera)  # set the values in ms
+
+            # print state
+            print(mufpga.get_camera_state_ms())
+
+            # define three lasers.
+            laser0 = {
+                'channel': 0,
+                'mode': LaserTriggerMode.MODE_FOLLOW,
+                'duration': 1,
+                'sequence':  sig.MAX_SEQUENCE
+            }
+            mufpga.set_laser_state(**laser0)
+
+            # we also need to start the camera
+            mufpga.start_camera()
+            print('Camera running')
+
+            # now the FPGA generates both camera and laser trigger for 2 s
+            time.sleep(2)  # in s
+
+            # stop, the trigger signals are off
+            mufpga.stop_camera()
+            print('Camera stopped')
+
+    else:
+        print('Failed to connect')
